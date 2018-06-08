1903d25f02f2d628a0a7e52f2306604fadf06eaa
Chris Dusyk <chris.dusyk@gmail.com>
2018-06-07 22:20:31 -0600

    Added TestMultipleOnePairWinners test
    Needed to cover scenario where multiple players win in tests

--- Poker.Tests/PokerTests.cs
+++ Poker.Tests/PokerTests.cs
@@ -208,5 +208,71 @@ namespace Poker.Tests
 			Assert.AreEqual("Devin", winners.FirstOrDefault().PlayerName);
 			Assert.AreEqual(HandTypes.HighCard, hand);
 		}
+
+		[TestMethod]
+		public void TestMultipleOnePairWinners()
+		{
+			IPlayerRepository playerRepository = new PlayerRepository();
+			IGameManager gameManager = new GameManager();
+
+			// Create test data
+			playerRepository.Add(new Player
+			{
+				PlayerName = "Devin",
+				Cards = new List<Card>
+				{
+					new Card { Rank = "A", Suit = "D" },
+					new Card { Rank = "2", Suit = "D" },
+					new Card { Rank = "3", Suit = "D" },
+					new Card { Rank = "7", Suit = "S" },
+					new Card { Rank = "10", Suit = "H" }
+				}
+			});
+
+			playerRepository.Add(new Player
+			{
+				PlayerName = "Maks",
+				Cards = new List<Card>
+				{
+					new Card { Rank = "K", Suit = "H" },
+					new Card { Rank = "Q", Suit = "D" },
+					new Card { Rank = "10", Suit = "S" },
+					new Card { Rank = "9", Suit = "D" },
+					new Card { Rank = "7", Suit = "H" }
+				}
+			});
+
+			playerRepository.Add(new Player
+			{
+				PlayerName = "Sally",
+				Cards = new List<Card>
+				{
+					new Card { Rank = "A", Suit = "C" },
+					new Card { Rank = "10", Suit = "C" },
+					new Card { Rank = "5", Suit = "C" },
+					new Card { Rank = "4", Suit = "D" },
+					new Card { Rank = "4", Suit = "S" }
+				}
+			});
+
+			playerRepository.Add(new Player
+			{
+				PlayerName = "Marge",
+				Cards = new List<Card>
+				{
+					new Card { Rank = "2", Suit = "D" },
+					new Card { Rank = "2", Suit = "S" },
+					new Card { Rank = "J", Suit = "H" },
+					new Card { Rank = "Q", Suit = "D" },
+					new Card { Rank = "K", Suit = "D" }
+				}
+			});
+
+			(var winners, var hand) = gameManager.GetWinners(playerRepository.GetAll());
+
+			// Assert that Sally won with One Pair
+			Assert.AreEqual(2, winners.Count);
+			Assert.AreEqual(HandTypes.OnePair, hand);
+		}
 	}
 }
